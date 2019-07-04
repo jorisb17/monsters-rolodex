144da27627071d14370072d91331fb49a7e24b18
Joris Bruinsma <joris@jorisb.nl>
2019-07-04 13:21:34 +0200

    Updates

---
+++ precache-manifest.541c139449fe7c192c2f9445fef32e39.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cda8aab2ea1ced629d5a9b9822add1ae",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "ddc0d0690ba92d9563d4",
+    "url": "/monsters-rolodex/static/css/main.45b88e6b.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "ddc0d0690ba92d9563d4",
+    "url": "/monsters-rolodex/static/js/main.4a9b0f23.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
